d1f4870b21c492f2abd80664e66645a240a085b7
richwwm <richwwm@gmail.com>
2016-06-21 16:32:22 +0800

    init

---
+++ EmailLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Emailapp/AppMain.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using Syqosis.Mail;
+
+namespace Emailapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void test()
+        {
+            
+        }
+    }
+}
